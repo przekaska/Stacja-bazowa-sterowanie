a5be361289f8bf0b8c00416c2b77ce20c9258d09
bati0 <78078810+bati0@users.noreply.github.com>
2024-07-18 17:53:35 +0200

    działa, wywalone automatyczne przełączanie, by połączyć wypada się nauczyć działania Zephyr RTOS i threads

--- src/main.c
+++ src/main.c
@@ -15,6 +15,10 @@
 #define I2C1 ""
 #endif
 
+/* The devicetree node indentifire for the "sw0" alias*/
+#define SW0_NODE    DT_ALIAS(sw0)
+
+
 #if DT_NODE_HAS_STATUS(SW0_NODE, okay)
 #define SW0_GPIO_LABEL  DT_GPIO_LABEL(SW0_NODE, gpios)
 #define SW0_GPIO_PIN    DT_GPIO_PIN(SW0_NODE, gpios)
@@ -25,28 +29,63 @@
 #define SW0_GPIO_PIN    0
 #define SW0_GPIO_FLAGS  0
 #endif
-
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
 #define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
+#define PIN		DT_GPIO_PIN(LED0_NODE, gpios)
 #define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
 #else
 /* A build error here means your board isn't set up to blink an LED. */
 #error "Unsupported board: led0 devicetree alias is not defined"
 #define LED0	""
-#define PIN	0
+#define PIN		0
 #define FLAGS	0
 #endif
+
+int i = 1;
+
+void button_pressed1(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+	gpio_pin_toggle(dev,PIN);
+	i = i+1;
+	printk("gowno i = %x \n", i);
+}
+/* Define a variable of type static struct gpio_callback */
+static struct gpio_callback button_cb_data;
 
 void main(void)
 {
 	int ret;
 	uint8_t antena_setting[2];
 	
-	/* STEP 6 - Get the binding of the I2C driver  */
+	const struct device *dev;
+
+	dev = device_get_binding(LED0);
+	if (dev == NULL) {
+		return;
+	}
+
+	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
+	if (ret < 0) {
+		return;
+	}
+
+
+	ret = gpio_pin_configure(dev, SW0_GPIO_PIN, GPIO_INPUT | SW0_GPIO_FLAGS);
+    if (ret < 0) {
+        return;
+    }
+
+	ret = gpio_pin_interrupt_configure(dev,SW0_GPIO_PIN,GPIO_INT_EDGE_TO_ACTIVE | GPIO_INT_DEBOUNCE);	
+	/* STEP 6 - Initialize the static struct gpio_callback variable   */
+	gpio_init_callback(&button_cb_data, button_pressed1, BIT(SW0_GPIO_PIN));	
+	/* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+	gpio_add_callback(dev, &button_cb_data);	
+	
+	/*Get the binding of the I2C driver  */
 	const struct device *dev_i2c = device_get_binding(I2C1);
 	if (dev_i2c == NULL) {
 		printk("Could not find  %s!\n\r",I2C1);
@@ -84,12 +123,10 @@ void main(void)
 	if(ret != 0){
 		printk("Failed to write input on bank A to I2C device address %x at Reg. %x \n", MCP23018_I2C_ADDR,antena_setting[0]);
 	}
-	
-	
-	while (1) {
-		for (int i = 1; i <= 12; i++)
-		{
-			switch (i)
+
+	while (1)
+	{
+		switch (i)
 			{
 			case 1:
 				antena_setting[0]= MCP23018_I2C_OLATA_ADDR;
@@ -279,31 +316,34 @@ void main(void)
 					printk("Failed to write values to OLATB %x at Reg. %x \n", MCP23018_I2C_ADDR,antena_setting[0]);
 				}
 				printk("{D, D, D, D, R, R, R, R, R, R, R, D}, // Characteristic 12 \n");
-				
+
 				break;
 			
 			default:
+				printk("error wartosc poza zakresem: %x\n",i);
 				break;
 			}
-			
-			//fragment wypisujący aktualną wartość na bankach
-			
-			uint8_t GPIOs_regs[2] ={MCP23018_I2C_OLATA_ADDR,MCP23018_I2C_OLATB_ADDR};
-			uint8_t val_reading[2]= {0};	
-			ret = i2c_write_read(dev_i2c,MCP23018_I2C_ADDR,&GPIOs_regs[0],1,&val_reading[0],1);
-			if(ret != 0){
-				printk("Failed to write/read I2C device address %x at Reg. %x \n", MCP23018_I2C_ADDR,GPIOs_regs[0]);
-			}
-			ret = i2c_write_read(dev_i2c,MCP23018_I2C_ADDR,&GPIOs_regs[1],1,&val_reading[1],1);
-			if(ret != 0){
-				printk("Failed to write/read I2C device address %x at Reg. %x \n", MCP23018_I2C_ADDR,GPIOs_regs[1]);
-			}
-			printk("Zapisane bank A = %x \n", val_reading[1]);
-			printk("Zapisane bank B = %x \n\n", val_reading[0]);
 
-			k_msleep(SLEEP_TIME_MS);
+		//fragment wypisujący aktualną wartość na bankach
+
+		uint8_t GPIOs_regs[2] ={MCP23018_I2C_OLATA_ADDR,MCP23018_I2C_OLATB_ADDR};
+		uint8_t val_reading[2]= {0};	
+		ret = i2c_write_read(dev_i2c,MCP23018_I2C_ADDR,&GPIOs_regs[0],1,&val_reading[0],1);
+		if(ret != 0){
+			printk("Failed to write/read I2C device address %x at Reg. %x \n", MCP23018_I2C_ADDR,GPIOs_regs[0]);
+		}
+		ret = i2c_write_read(dev_i2c,MCP23018_I2C_ADDR,&GPIOs_regs[1],1,&val_reading[1],1);
+		if(ret != 0){
+			printk("Failed to write/read I2C device address %x at Reg. %x \n", MCP23018_I2C_ADDR,GPIOs_regs[1]);
+		}
+		printk("Zapisane bank A = %x \n", val_reading[1]);
+		printk("Zapisane bank B = %x \n\n", val_reading[0]);
+		k_msleep(SLEEP_TIME_MS);
+		if (i>12){
+			i=1;
 		}
 	}
+
 }
 
 // ## Pre-defined characteristics - plik excel 
